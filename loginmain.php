f896fff09df334194a558a9484f8f5ef9c8d99e8
brayanj4y <souopsylvain@gmail.com>
2024-12-16 01:46:55 -0700

    Update style

--- loginmain.php
+++ loginmain.php
@@ -15,7 +15,7 @@
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="loginstyle.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/loginstyle.css">
 </head>
 <body>
      <form action="login.php" method="post">
